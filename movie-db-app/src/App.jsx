62384b1dfc29514cc873add9b4bcc5c72099efd7
C7-mia <akidirita4@gmail.com>
2025-09-06 18:51:03 +0300

    Update App.jsx

--- movie-db-app/src/App.jsx
+++ movie-db-app/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       </main>
 
       <footer className="text-center py-4 mt-8 bg-gray-800 text-gray-500 text-sm">
-        <p>Powered by OMDB API. Created for Capstone Project.</p>
+        <p>Powered by OMDB API. Best of Entertainment .</p>
       </footer>
     </div>
   );
