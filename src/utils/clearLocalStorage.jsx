1ab1fc1087882e13c196ce75fa0525e9cc484ff9
C7-mia <akidirita4@gmail.com>
2025-09-06 14:16:39 +0300

    Delete src directory

--- src/utils/clearLocalStorage.jsx
+++
@@ -1,6 +0,0 @@
-(function clearLocalStorageOnStart() {
-  if (process.env.REACT_APP_CLEAR_STORAGE === "true") {
-    console.log("🧹 Clearing localStorage...");
-    localStorage.clear();
-  }
-})();
